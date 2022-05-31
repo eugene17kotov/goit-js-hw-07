f0f5b9afafdb59ab4ee027c5e61a79c52c2862fb
Yevhenii Kobets <eu.kobets@gmail.com>
2022-06-01 00:01:24 +0300

    raw product

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+galleryRef.addEventListener('click', onGalleryImageClick);
+
+let lightboxGallery;
+
+function createGallery(array) {
+    const galleryItemsMarkup = getGalleryItemsMarkup(array);
+    getGalleryItemsToHtml(galleryItemsMarkup);
+}
+
+createGallery(galleryItems);
+
+function getGalleryItemsMarkup(array) {
+    return array
+        .map(({ preview, original, description }) => {
+            return `
+    <a class="gallery__link" href=${original}>
+        <img
+            class="gallery__image"
+            src=${preview}
+            alt=${description}
+        />
+    </a>`;
+        })
+        .join('');
+}
+
+function getGalleryItemsToHtml(markup) {
+    galleryRef.innerHTML = markup;
+}
+
+function onGalleryImageClick(e) {
+    e.preventDefault();
+
+    if (e.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    lightboxGallery = new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionDelay: 250,
+    });
+}
