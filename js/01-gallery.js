901bf72d4997d03b3fde3fb199ff4240f4cdcba2
Yevhenii Kobets <eu.kobets@gmail.com>
2022-06-01 15:49:55 +0300

    clear comments

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,12 +1,5 @@
 import { galleryItems } from './gallery-items.js';
 
-// DONE 1) Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
-// DONE 2) Реализация делегирования на div.gallery и получение url большого изображения.
-// DONE 3) Подключение скрипта и стилей библиотеки модального окна basicLightbox с помощью CDN сервис jsdelivr (.min)
-// DONE 4) Открытие модального окна по клику на элементе галереи.
-// DONE 5) Замена значения атрибута src элемента < img > в модальном окне перед открытием. Использовать готовую разметку библиотеки basicLightbox
-// DONE 6) Добавь закрытие модального окна по нажатию клавиши Escape
-
 const galleryRef = document.querySelector('.gallery');
 
 galleryRef.addEventListener('click', onGalleryImageClick);
